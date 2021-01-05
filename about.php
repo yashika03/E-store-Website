623e75ebc29721532b0cb82b6adb663f6e460eb2
yashika03 <76561509+yashika03@users.noreply.github.com>
2021-01-05 12:58:23 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <!--jQuery library--> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!--Latest compiled and minified JavaScript--> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title>
+            Information about E-Store
+        </title>
+        <style>
+            #footer_style{
+            padding:10px 0;
+            background-color:#101010;
+            color:#9d9d9d;
+            bottom:0;
+            width:100%;}
+        </style>
+    </head>
+    <body>
+        <?php
+        include 'header_logged_out.php';
+        ?>
+        <div class="container" style="margin-top: 80px; margin-bottom: 50px;">
+            <div class="row row_style">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <h3><b><i><u>WHO WE ARE</u></i></b></h3><br>
+                        <br>
+                        <img src="call1.jpg" alt="image"><br><br>
+                        <p>E-Store is an Indian Electronic Commerce Company with headquarters in Gurugram , Haryana. 
+                            It is the largest internet based retailer in India. 
+                            E-Store started as an online blog, but soon diversified, selling mobile phones.
+                            E-store has a separate retail website different regions of the country and also ship our products to different countries like United States of America, UK, Germany, France, Canada, UAE, Japan, South Korea, Singapore etc. 
+                            Amazon charges international charges for certain products when delivering internationally, but we provide products free of delivery charges across the globe without a single penny. 
+                            We also have branches in many countries and we provide facility of product to be returned in 30 days. 
+                            We deal with the best manufactures so we provide quality products to our costumers.
+                        </p>
+                </div>
+                 <div class="col-md-4 col-sm-6 col-xs-12">
+                     <h3><b><i><u>OUR HISTORY</u></i></b></h3><br>
+                        <br>
+                        <a href="#" >-1995-</a>
+		        <p>Company was founded by Mr. Sameer Bhatia inventor, innovator and entrepreneur in 1995, credited with developing the predecessor to online shopping. Bhatia left his job at Apple and moved to Gurugram with his wife. He began to work on a business plan for what would eventually become E-Store.</p>
+
+			<a href="#">-2000-</a>
+			<p>In the late 2000, E-store was started to become popular. People began to shop online and in the same year E-store expanded its limits and switched to a fully electronic product based firm.</p>
+
+		        <a href="#">-2008-</a> 
+			<p>The era of internet has begun and people started to depend more on internet than anything else and that brought to a golden opportunity to online stores. E-store bacame the second major online ecommerce store and was awarded by many highly reputed magazines.</p>
+
+			<a href="#">-2018-</a>
+			<p>Now, E-store has became the world wide best #1 ecommerce site and is mostly trusted by commom man, as we provide best products at the lowest cost possible. E-store will always be there for it's costumers.</p>
+                </div>
+                 <div class="col-md-4 col-sm-6 col-xs-12">
+                     <h3><b><i><u>OPPORTUNITIES</u></i></b></h3><br>
+                        <br>
+                        <b>Available Rows</b><br>
+                        <ol>
+                            <li>Jr./Sr. Web Developer [Full Time Role + also available as a 6 month internship]</li><br>
+			    <li>Business Apprentice [6 month internship]</li><br>
+			    <li>Manager at back-end operations [Full Time Role + also available as a 6 month internship]</li>
+                        </ol>
+                </div>
+            </div>
+        </div>
+        <?php
+        include 'footer.php';
+        ?>
+    </body>
+</html>
